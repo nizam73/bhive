ec830bd62b29fee88879cdcb81a32869c0bf2a11
nizam73 <ahmed.nizam73@gmail.com>
2026-04-20 13:37:21 +0600

    Refactor server.js to use environment port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { RouterOSClient } = require('routeros-client');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -55,6 +57,7 @@ app.post('/create-user', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
